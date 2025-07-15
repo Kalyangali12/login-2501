8ea5bbdb6658c8479917a559ab8f2e8613b5c3ff
kalyangali12 <galikalyan12@gmail.com>
2025-07-15 14:35:42 +0530

    create aws provider

---
+++ terraform/variable.tf
@@ -0,0 +1,12 @@
+# Variables
+
+# AWS access key ID
+variable aws_access_key {
+  description = "Please Input AWS Access Key ID"
+}
+
+# AWS Security key ID
+variable aws_Security_key {
+  description = "Please Input AWS Security Key ID"
+}
+
